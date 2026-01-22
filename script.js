9f66ebb034977268b3dbf95afaf208eead37f7c6
Lexx Bor <alex2007bor@gmail.com>
2026-01-22 23:02:05 +0300

    add adaptive

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let leftHand = document.querySelector('#left')
     let draggable = false
     let offsetX = 0
+    let offsetY = 0 // 
     let triggerSound = new Audio("plankton.mp3")
     let isLinked = false
     function playTriggerSound() {
@@ -18,6 +19,7 @@ document.addEventListener('DOMContentLoaded', () => {
             isLinked = false
             leftHand.style.transition = ".2s ease-out"
             leftHand.style.left = "0px"
+            leftHand.style.top = "" // 
             leftHand.style.transition = "none"
         }, 4700)
     }
@@ -32,23 +34,33 @@ document.addEventListener('DOMContentLoaded', () => {
     rightHand.addEventListener('pointerdown', (e) => {
         draggable = true
         rightHand.classList.remove('static')
-        rightHand.style.left = `${e.clientX - rightHand.getBoundingClientRect().width/2}px`
+
         let rect = rightHand.getBoundingClientRect()
+
         offsetX = e.clientX - rect.left
+        offsetY = e.clientY - rect.top //
+
+        rightHand.style.left = `${e.clientX - rightHand.getBoundingClientRect().width/2}px`
+        rightHand.style.top = `${e.clientY - rightHand.getBoundingClientRect().height/2}px`; 
     })
 
     rightHand.addEventListener('pointermove', (e) => {
         if(draggable){
             let rect = rightHand.getBoundingClientRect()
             let maxLeft = window.innerWidth - rect.width
+            let maxTop = window.innerHeight - rect.height; //
             let desiredLeft = e.clientX - offsetX
+            let desiredTop = e.clientY - offsetY //
             let clampedLeft = Math.min(Math.max(desiredLeft, 0), maxLeft) //!
+            let clampedTop = Math.min(Math.max(desiredTop, 0), maxTop)
             rightHand.style.left = `${clampedLeft}px`
+            rightHand.style.top = `${clampedTop}px`
             toggleSound()
             linkedToParent()
             if(isLinked){
                 console.log('yes')
                 leftHand.style.left = `${rightHand.getBoundingClientRect().left - leftHand.getBoundingClientRect().width + 50}px`
+                leftHand.style.top = `${rightHand.getBoundingClientRect().top}px`
             }
         }
     })
